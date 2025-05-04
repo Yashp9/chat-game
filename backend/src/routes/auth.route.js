a80649f2b5efb5bc432c139fe65a27515c751b16
unknown <pandeyyash058@gmail.com>
2025-05-04 23:20:53 +0530

    resolve toast error

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,7 +6,7 @@ import rateLimit from "express-rate-limit";
 const loginLimiter = rateLimit({
     windowMs:5 * 60 * 1000, //5  minutes
     max:5,// limit to 5 login attempts
-    message:"Too many login attempts. Please try again in 5 minutes.",
+    message:{message:"Too many login attempts. Please try again in 5 minutes."},
 });
 
 const router = express.Router();
